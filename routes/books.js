5943c91a113c8e25815539ea6ecbf9ef8fb169a8
akshatha-27 <nayakakshatha27@gmail.com>
2025-06-30 19:20:23 +0530

    Initial commit

--- routes/books.js
+++ routes/books.js
@@ -1,62 +1,24 @@
 const express = require("express");
-const router = express.Router();
+const {
+  getAllBooks,
+  getSingleBookById,
+  getAllIssuedBooks,
+} = require("../controllers/book-controller");
 const { books } = require("../data/books.json");
 const { users } = require("../data/users.json");
+
+const router = express.Router();
 
 const { UserModel, BookModel } = require("../models/index");
 
 //Getting all books
-router.get("/", (req, res) => {
-  res.status(200).json({
-    success: true,
-    data: books,
-  });
-});
+router.get("/", getAllBooks);
 
 //Get all issued books
-router.get("/issued", (req, res) => {
-  const usersWithIsuuedBook = users.filter((each) => {
-    if (each.issuedBook) return each;
-  });
-  const issuedBooks = [];
-  usersWithIsuuedBook.forEach((each) => {
-    const book = books.find((book) => book.id === each.issuedBook);
-    book.issuedBy = each.name;
-    book.issuedDate = each.issuedDate;
-    book.returnDate = each.returnDate;
-
-    issuedBooks.push(book);
-  });
-  if (issuedBooks.length === 0) {
-    return res.status(404).json({
-      success: false,
-      message: "No Book Has Been Issued Yet",
-    });
-  }
-  return res.status(200).json({
-    success: true,
-    message: "Users with issued books",
-    data: issuedBooks,
-  });
-});
+router.get("/issued", getAllIssuedBooks);
 
 //Getting a book by ID
-router.get("/:id", (req, res) => {
-  //const id=req.params.id;
-  const { id } = req.params;
-  const book = books.find((each) => each.id === id);
-  if (!book) {
-    return res.status(404).json({
-      success: false,
-      message: "Book Not Found",
-    });
-  }
-  res.status(200).json({
-    success: true,
-    message: "Book Found",
-    data: book,
-  });
-});
+router.get("/:id", getSingleBookById);
 
 //Adding a new book
 router.post("/", (req, res) => {
